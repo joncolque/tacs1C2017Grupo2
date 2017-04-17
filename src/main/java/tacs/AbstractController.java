8fabf1475a903c54e77d8f2f54cb09c34f7d9135
Alvaro <alvaro.arando@gmail.com>
2017-04-17 13:19:33 -0300

    ranking

--- src/main/java/tacs/AbstractController.java
+++ src/main/java/tacs/AbstractController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
-public abstract class AbstractController extends Connection {
+public abstract class AbstractController {
 
 	protected final String BASE_URL = "https://api.themoviedb.org/3/";
 	protected final String API_KEY = "api_key=3eb489d424860bc6870dc6776d05f6b9";
